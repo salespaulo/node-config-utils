8599c0104f27cdcc8d528bede8f724ae13c75f98
Paulo R. A. Sales <salespaulo08@gmail.com>
2019-06-20 19:40:10 -0300

    Improve dirs and includes flatmap and asyncFilter

--- src/app/arrays/index.js
+++ src/app/arrays/index.js
@@ -1,18 +1,22 @@
+const { toJson, toBase64 } = require('../objects')
 
-const { toJson, toBase64 } = require('./objects')
+const flatmap = (f, a) => {
+    return a.map(f).reduce((xs, ys) => [...xs, ...ys])
+}
 
 /*
- * Para utilizar com arrays e funcoes async, o filter nao funciona no
+ * Para utilizar com arrays e funcoes async,
+ * o filter do array nao funciona no
  * contexto async.
  */
 const asyncFilter = async (asyncPredicate, xs) => {
     return await xs.reduce(async (acc, x) => {
         const result = await asyncPredicate(x)
-        if (result ) {
-            (await acc).push(x)
+        if (result) {
+            ;(await acc).push(x)
         }
         return acc
-    }, Promise.resolve([ ]))
+    }, Promise.resolve([]))
 }
 
 const arrayToJson = array => {
@@ -24,6 +28,7 @@ const arrayToBase64 = array => {
 }
 
 module.exports = {
+    flatmap,
     asyncFilter,
     arrayToBase64,
     arrayToJson
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,18 @@
-const utils = require('./utils')
-const arrays = require('./utils/arrays')
-const objects = require('./utils/objects')
+'use strict'
+
+const arrays = require('./arrays')
+const objects = require('./objects')
+
+const IS_ENV_DEV = () => process.env.NODE_ENV === 'development'
+const IS_ENV_TEST = () => process.env.NODE_ENV === 'test'
+const IS_ENV_PROD = () => process.env.NODE_ENV === 'production'
+const IS_ENV_LOCALHOST = () => process.env.NODE_ENV === 'test-localhost'
 
 module.exports = {
-    ...utils,
+    IS_ENV_DEV,
+    IS_ENV_TEST,
+    IS_ENV_PROD,
+    IS_ENV_LOCALHOST,
     arrays,
     objects
 }
